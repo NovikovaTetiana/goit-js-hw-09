f45a6696f73f3ddccc22789bc0dde3777e37d33f
NovikovaTetiana <113465579+NovikovaTetiana@users.noreply.github.com>
2023-03-23 21:11:55 +0000

    Deploying to gh-pages from @ NovikovaTetiana/goit-js-hw-09@7ad558a1c3b33e5ff62ead0cbc4c03820dae1def 🚀

--- 01-color-switcher.be8173d4.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.querySelector("body"),o=null;function r(){return"#".concat(Math.floor(16777215*Math.random()).toString(16))}t.addEventListener("click",(function(){o=setTimeout((function(){n.style.backgroundColor=r(),t.disabled=!0,e.disabled=!1,o=setInterval((function(){n.style.backgroundColor=r()}),1e3)}))}),0),e.addEventListener("click",(function(){clearInterval(o),t.disabled=!1,e.disabled=!0}))}();
-//# sourceMappingURL=01-color-switcher.be8173d4.js.map
